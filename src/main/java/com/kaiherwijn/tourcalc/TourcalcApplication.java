3d3bd996c9eb7324a14e362411f38fb3468b2cb9
JavanKadd <kai.herwijn@additude.eu>
2022-08-25 15:53:21 +0200

    Initiële commit

---
+++ src/main/java/com/kaiherwijn/tourcalc/TourcalcApplication.java
@@ -0,0 +1,13 @@
+package com.kaiherwijn.tourcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourcalcApplication.class, args);
+	}
+
+}
